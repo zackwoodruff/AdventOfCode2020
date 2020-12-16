e1dcddf581396e0d55be4bc9cd0638d016d0c9e8
Zack Woodruff <jzwoodruff@u.northwestern.edu>
2020-12-16 10:09:42 -0800

    Include part 2 solution

--- day2/day2.cpp
+++ day2/day2.cpp
@@ -17,23 +17,35 @@ int main(){
 	char ch; 
 	char password[100];
 	
-	int n_valid = 0; 
+	int n_valid_part1 = 0;
+	int n_valid_part2 = 0; 
+	
 	while(std::getline(infile,input)){
 		//Example input: 2-6 w: wkwwwfwwpvw
 		std::sscanf(input.c_str(),"%d-%d %c: %s",&min, &max, &ch, password);
 		
-		// Part 1: Check passwords against rules
+		// Part 1: Check passwords against part 1 rules
 		int n_char = 0;
-		for (int i=0; password[i] != '\0'; ++i){
+		int i;
+		for (i=0; password[i] != '\0'; ++i){
 			if (ch==password[i]){
 				++n_char; 
 			}	
 		}
 		if (n_char >= min && n_char <= max){
-			++n_valid;
+			++n_valid_part1;
 		}
+		
+		// Part 2: Check passwords against part 2 rules
+		if((password[min-1]==ch && password[max-1]!=ch) || (password[min-1]!=ch && password[max-1]==ch)){
+			++n_valid_part2;
+		}
+		
 	}
-	cout << n_valid << endl; 
+	
+	// Output number of valid passwords for parts 1 and 2
+	cout << "Part 1: " << n_valid_part1 << endl; 
+	cout << "Part 2: " << n_valid_part2 << endl; 
 	
 	return 0;
 }
