10718d33f419a5a450377fbb547c70fca6e561db
Zack Woodruff <jzwoodruff@u.northwestern.edu>
2020-12-16 09:14:07 -0800

    Add day 2 input file and initial commit

---
+++ day2/day2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using std::cout; 
+using std::endl;
+
+int main(){
+	cout << "Day 2!" << endl;
+	
+	// Read/Parse Input
+	
+	// Part 1: Check passwords against rules
+	
+	return 0;
+}
